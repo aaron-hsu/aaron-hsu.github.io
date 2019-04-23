f3eaf29169bcc4883248db95550b938e5398d28f
Aaron Hsu <ice.aaron@gmail.com>
2019-04-23 20:12:25 +0800

    Create ad2.js

---
+++ ad2.js
@@ -0,0 +1,60 @@
+(function() {
+    // Create the connector object
+    var myConnector = tableau.makeConnector();
+
+    // Define the schema
+    myConnector.getSchema = function(schemaCallback) {
+        var cols = [{
+            id: "date",
+            dataType: tableau.dataTypeEnum.date
+        }, {
+            id: "cost",
+            dataType: tableau.dataTypeEnum.float
+        }, {
+            id: "download",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "click",
+            dataType: tableau.dataTypeEnum.float
+        }];
+
+        var tableSchema = {
+            id: "AD",
+            alias: "AD",
+            columns: cols
+        };
+
+        schemaCallback([tableSchema]);
+    };
+
+    // Download the data
+    myConnector.getData = function(table, doneCallback) {
+        $.getJSON("https://sheetsu.com/apis/v1.0su/edf14537c4b2", function(resp) {
+            var feat = resp.features,
+                tableData = [];
+
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                tableData.push({
+                    "id": feat[i].id,
+                    "mag": feat[i].properties.mag,
+                    "title": feat[i].properties.title,
+                    "location": feat[i].geometry
+                });
+            }
+
+            table.appendRows(tableData);
+            doneCallback();
+        });
+    };
+
+    tableau.registerConnector(myConnector);
+
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
+        $("#submitButton").click(function() {
+            tableau.connectionName = "AD"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        });
+    });
+})();
